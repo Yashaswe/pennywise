3dbc936b789528dbc14477fe24803f18fa03259e
Yashaswe <amatyayasu@gmail.com>
2024-10-26 16:56:57 -0400

    add ant-design and dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import { useState, useEffect } from "react";
 
-import "./App.css";
+// import "./App.css";
 import axios from "axios";
 
-function App() {
+import React from "react";
+import { Breadcrumb, Layout, Menu, theme } from "antd";
+const { Header, Content, Footer } = Layout;
+const items = new Array(3).fill(null).map((_, index) => ({
+  key: String(index + 1),
+  label: `nav ${index + 1}`,
+}));
+
+const App = () => {
   const [jokes, setJokes] = useState([]);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
 
   useEffect(() => {
     axios
@@ -16,19 +27,77 @@ function App() {
         console.log(error);
       });
   }, []);
-
   return (
-    <>
-      <h1>Yahs</h1>
-      <p>Jokes:{jokes.length}</p>
-      {jokes.map((joke, index) => (
-        <div key={joke.id}>
-          <h3>{joke.title}</h3>
-          <p>{joke.content}</p>
+    <Layout>
+      <Header
+        style={{
+          position: "sticky",
+          top: 0,
+          zIndex: 1,
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={items}
+          style={{
+            flex: 1,
+            minWidth: 0,
+          }}
+        />
+      </Header>
+      <Content
+        style={{
+          padding: "0 48px",
+        }}
+      >
+        <Breadcrumb
+          style={{
+            margin: "16px 0",
+          }}
+        >
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div
+          style={{
+            padding: 24,
+            minHeight: 380,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          Content
         </div>
-      ))}
-    </>
+      </Content>
+      <Footer
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Ant Design ©{new Date().getFullYear()} Created by Ant UED
+      </Footer>
+    </Layout>
   );
-}
+
+  // return (
+  //   <>
+  //     <h1>Yahs</h1>
+  //     <p>Jokes:{jokes.length}</p>
+  //     {jokes.map((joke, index) => (
+  //       <div key={joke.id}>
+  //         <h3>{joke.title}</h3>
+  //         <p>{joke.content}</p>
+  //       </div>
+  //     ))}
+  //   </>
+  // );
+};
 
 export default App;
